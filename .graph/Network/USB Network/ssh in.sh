9c9e46f65afa8faf7ee214fe513a26c1eb83d2f1
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-05-16 19:04:33 -0500

    Update ssh in.sh

--- .graph/Network/USB Network/ssh in.sh
+++ .graph/Network/USB Network/ssh in.sh
@@ -1,7 +1,14 @@
 # Doc: https://wiki.postmarketos.org/wiki/USB_Network
 
-```ssh linux@172.16.42.1
+```
+connect(){
+  ssh linux@172.16.42.1
+}
+```
 
-uname -a
-lscpu
+```
+test(){
+  uname -a
+  lscpu
+}
 ```
